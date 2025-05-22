b4a691089f83163a7251345a8f449860f4d91d0c
russllist <russell.cronin@gmail.com>
2025-05-22 12:05:33 -0600

    Design main page structures

---
+++ app/SidePanel.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SidePanel = () => {
+  return (
+    <div className="bg-amber-200 w-50 space-y-4 px-4 py-4">
+      <p>This is the side panel </p>
+      <p>Sorting Dropdown</p>
+      <p>Filters</p>
+    </div>
+  );
+};
+
+export default SidePanel;
