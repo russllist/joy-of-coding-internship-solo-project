8537ed845a6cc17ba07af7fef0144ec015ae5b76
russllist <russell.cronin@gmail.com>
2025-08-06 12:33:08 -0600

    Replace placeholder data with database data

--- app/components/ReplayTile.tsx
+++ app/components/ReplayTile.tsx
@@ -2,17 +2,22 @@ import React, { PropsWithChildren } from 'react';
 import { Replay } from '../page';
 
 const ReplayTile = ({ replay }: { replay: Replay }) => {
-    return (
-        <div className="bg-gray-300 p-2 m-1 rounded-md">
-            <p>{replay.url}</p>
-            <p>{replay.thumbnail}</p>
-            <p>{replay.title}</p>
-            <p>{replay.host}</p>
-            <p>{replay.date}</p>
-            <p>{replay.duration}</p>
-            <p>{replay.description}</p>
-        </div>
-    );
+  return (
+    <div className="bg-gray-300 p-2 m-1 rounded-md flex space-x-4">
+      <a href={replay.url}>
+        <img src={replay.thumbnail} style={{ height: '120px' }} />
+      </a>
+      <div>
+        <a href={replay.url}>
+          <h2>{replay.title}</h2>
+        </a>
+        <p>{replay.host}</p>
+        {/* <p>{replay.date}</p>
+            <p>{replay.duration}</p> */}
+        <p>{replay.description}</p>
+      </div>
+    </div>
+  );
 };
 
-export default TaskTile;
+export default ReplayTile;
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,65 @@
+// 'use client';
 import React from 'react';
-import TaskTile from './components/TaskTile';
-
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-}
+import ReplayTile from './components/ReplayTile';
 
 export interface Replay {
-    id: number;
-    url: string;
-    thumbnail: string;
-    title: string;
-    host: string;
-    date: Date;
-    duration: Date;
-    description: string;
+  id: number;
+  url: string;
+  thumbnail: string;
+  title: string;
+  host: string;
+  date: Date;
+  duration: Date;
+  description: string;
 }
 
 const page = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-        next: { revalidate: 10 },
-    });
-    const users: User[] = await res.json();
-    // client side filtering
-    const filtered: User[] = users.filter(
-        (user) => user.name === 'Leanne Graham'
-    );
+  const replayRes = await fetch(
+    'http://localhost:3000/api/replays'
+    //   const replayRes = await fetch('/api/replays', {
+    // next: { revalidate: 10 },
+  );
+  const replays: Replay[] = await replayRes.json();
 
-    return (
-        <div>
-            <br />
+  // client side filtering
+  const filtered: Replay[] = replays.filter(
+    (replay) => replay.host === 'Katrina'
+  );
 
-            <h1>Placeholder Tiles fetching data from jsonplaceholder:</h1>
-            <div>
-                {users.map((user) => (
-                    <TaskTile key={user.id} user={user} />
-                ))}
-            </div>
-        </div>
-    );
+  // const replaysTemp = [
+  //   {
+  //     id: 1,
+  //     url: 'https://www.youtube.com/watch?v=1J-H-07jcBU',
+  //     thumbnail: 'https://i.ytimg.com/vi/1J-H-07jcBU/hqdefault.jpg',
+  //     title: '2025-06-21T18:48:53Z - Python Party & Dev-in-Training Updates',
+  //     host: 'Katrina',
+  //     date: '2025-06-21T00:00:00.000Z',
+  //     duration: '1970-01-01T18:48:53.000Z',
+  //     description: 'none yet',
+  //   },
+  //   {
+  //     id: 4,
+  //     url: 'https://www.youtube.com/watch?v=6GZ6sb_acgI',
+  //     thumbnail: 'https://i.ytimg.com/vi/6GZ6sb_acgI/hqdefault.jpg',
+  //     title: '2025-07-21T23:24:25Z - Life Coaching with Nando: Mindset Mondays',
+  //     host: 'Nando',
+  //     date: '2025-07-21T00:00:00.000Z',
+  //     duration: '1970-01-01T00:35:11.000Z',
+  //     description: 'none yet',
+  //   },
+  // ];
+
+  return (
+    <div>
+      <br />
+      <h1>Add Button and Sorting will go here</h1>
+      <div>
+        {replays.map((replay) => (
+          <ReplayTile key={replay.id} replay={replay} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default page;
