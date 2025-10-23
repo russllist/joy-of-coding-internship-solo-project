431ee0a4296474fdc041f5a5da94d36b64db275a
russllist <russell.cronin@gmail.com>
2025-10-23 14:39:53 -0500

    Clean dummy data from main page

--- app/page.tsx
+++ app/page.tsx
@@ -16,45 +16,15 @@ export interface Replay {
 }
 
 const page = async () => {
-  const replayRes = await fetch(
-    'http://localhost:3000/api/replays'
-    //   const replayRes = await fetch('/api/replays', {
-    // next: { revalidate: 10 },
-  );
+  const replayRes = await fetch('http://localhost:3000/api/replays');
   const replays: Replay[] = await replayRes.json();
-
-  // client side filtering
-  const filtered: Replay[] = replays.filter(
-    (replay) => replay.host === 'Katrina'
-  );
-
-  // const replaysTemp = [
-  //   {
-  //     id: 1,
-  //     url: 'https://www.youtube.com/watch?v=1J-H-07jcBU',
-  //     thumbnail: 'https://i.ytimg.com/vi/1J-H-07jcBU/hqdefault.jpg',
-  //     title: '2025-06-21T18:48:53Z - Python Party & Dev-in-Training Updates',
-  //     host: 'Katrina',
-  //     date: '2025-06-21T00:00:00.000Z',
-  //     duration: '1970-01-01T18:48:53.000Z',
-  //     description: 'none yet',
-  //   },
-  //   {
-  //     id: 4,
-  //     url: 'https://www.youtube.com/watch?v=6GZ6sb_acgI',
-  //     thumbnail: 'https://i.ytimg.com/vi/6GZ6sb_acgI/hqdefault.jpg',
-  //     title: '2025-07-21T23:24:25Z - Life Coaching with Nando: Mindset Mondays',
-  //     host: 'Nando',
-  //     date: '2025-07-21T00:00:00.000Z',
-  //     duration: '1970-01-01T00:35:11.000Z',
-  //     description: 'none yet',
-  //   },
-  // ];
+  // const filtered: Replay[] = replays.filter(
+  //   (replay) => replay.host === 'Katrina'
+  // );
 
   return (
     <div>
       <br />
-      <h1>Sorting will go here</h1>
       <Button>
         <Link href="/newreplay">Add New Replay</Link>
       </Button>
